43e3b8f75d3026dec9d74b69155a67be8465b1b0
lostleaf <lostleaf@me.com>
2022-06-22 01:42:27 +0800

    Delete deploy.sh

--- deploy.sh
+++
@@ -1,3 +0,0 @@
-git clone --depth=1 https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/vundle
-git clone --depth=1 https://github.com/altercation/vim-colors-solarized.git ~/.vim/bundle/vim-colors-solarized
-vim -c "BundleInstall" -c "qa!"
